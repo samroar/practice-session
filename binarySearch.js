9a780e55f97bc86aacd78b4d70882ebd6e162a6b
durbar <durbardasgupta.dd@gmail.com>
2017-10-31 18:50:44 +0530

    binary search tree

--- binarySearch.js
+++ binarySearch.js
@@ -16,3 +16,38 @@ function binarySearch(array, key) {
     }
     return -1;
 }
+
+
+BinarySearchTree.prototype.push = function(val){
+   var root = this.root;
+
+   if(!root){
+      this.root = new Node(val);
+      return;
+   }
+
+   var currentNode = root;
+   var newNode = new Node(val); 
+
+   while(currentNode){
+      if(val < currentNode.value){
+          if(!currentNode.left){
+             currentNode.left = newNode;
+             break;
+          }
+          else{
+             currentNode = currentNode.left;
+        }
+     }
+     else{
+         if(!currentNode.right){
+            currentNode.right = newNode;
+            break;
+         }
+         else{
+            currentNode = currentNode.right;
+         }
+     }
+  }
+
+}
